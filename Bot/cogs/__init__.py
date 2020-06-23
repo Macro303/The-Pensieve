bbc4e66f1a7ada84b91ce16ec2dfef1ede866934
Macro303 <Macro303@users.noreply.github.com>
2020-06-23 13:22:53 +1200

    Strip incoming messages

--- Bot/cogs/__init__.py
+++ Bot/cogs/__init__.py
@@ -5,4 +5,4 @@ def get_message(ctx) -> str:
     msg = ctx.message.content
     prefix_used = ctx.prefix
     alias_used = ctx.invoked_with
-    return msg[len(prefix_used) + len(alias_used):]
+    return msg[len(prefix_used) + len(alias_used):].strip()
